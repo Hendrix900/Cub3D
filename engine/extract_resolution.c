f129491443578c4d58e1f0e654290df0c48dfb13
ccastill <carlosc90@gmail.com>
2021-03-02 07:14:01 +0100

    1

--- engine/extract_resolution.c
+++ engine/extract_resolution.c
@@ -6,7 +6,7 @@
 /*   By: carlos <carlos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 21:54:22 by ccastill          #+#    #+#             */
-/*   Updated: 2021/03/02 04:23:24 by carlos           ###   ########.fr       */
+/*   Updated: 2021/03/02 05:02:16 by carlos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,13 @@ void	check_resolution(char *line)
 void	extract_resolution(char *line, char *position)
 {
 	char **split;
+	
 	check_resolution(line);
 	split = ft_split(position,' ');
 	count_split(split, 3);
 	if (!(g_check.resolution_width = ft_atoi(split[1])) ||
 	(!(g_check.resolution_hight = ft_atoi(split[2]))))
-		print_error("Introducido caracteres donde deberían ir números");
+		print_error("Wrong resolution number");
 	if (g_check.resolution_width > 1920 || g_check.resolution_hight > 1080)
 	{
 		g_check.resolution_width = 1920;
--- engine/extract_texture.c
+++ engine/extract_texture.c
@@ -6,24 +6,52 @@
 /*   By: carlos <carlos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 02:29:00 by ccastill          #+#    #+#             */
-/*   Updated: 2021/03/02 04:35:21 by carlos           ###   ########.fr       */
+/*   Updated: 2021/03/02 07:03:24 by carlos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-char *extract_texture(char *line, char *position)
+int	check_texture(char *path, char *xpm, char *png)
+{
+	int l;
+	int i;
+
+	l = 0;
+	i = ft_strlen(path)-4;
+	if ((path[0] != '.') && (path[1] != '/'))
+		print_error("Wrong texture path");
+	if (ft_strnstr(path, ".xpm", ft_strlen(path)))
+	{
+		while(path[i] != '\0')
+		{
+			if (xpm[l++] != path[i++])
+				print_error("Wrong texture path");
+		}
+	}
+	else
+	{
+		while(path[i] != '\0')
+		{
+			if (png[l++] != path[i++])
+				print_error("Wrong texture path");
+		}
+	}
+}
+
+char	*extract_texture(char *line, char *position)
 {
 	char **split;
 	char *path;
-	if (!(ft_strnstr(line, "./", ft_strlen(line))))
-		print_error("Wrong texture path");
-	if 	((!(ft_strnstr(line, ".xpm", ft_strlen(line)))) && // Cambiarlo
-		(!(ft_strnstr(line, ".png", ft_strlen(line)))))
-			print_error("Wrong texture path");
+	char *xpm;
+	char *png;
+	
+	xpm = ".xpm";
+	png = ".png";
 	split = ft_split(position, ' ');
 	count_split(split, 2);
 	path = ft_strdup(split[1]);
+	check_texture(path, xpm, png);
 	free(split);
 	g_check.count_parameters++;
 	return (path);
